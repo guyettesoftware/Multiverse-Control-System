0cf1b6496516b8833c4e236066139214508906d1
Yolo <yolo@gmail.com>
2022-11-30 12:12:26 -0600

    asdf

---
+++ Main.cs
@@ -0,0 +1,15 @@
+public static class Main
+{
+    public static void Main()
+    {
+        MyRepository.Branch
+    }
+}
+
+public static class MyRepository
+{
+    public static void MyMethod()
+    {
+        // Do something
+    }
+}
